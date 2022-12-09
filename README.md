d8882bcfeab55ffb9dff9e0c12d1d1a252637e80
Irreq <irreq@protonmail.com>
2022-12-09 21:05:15 +0100

    Code cleanup and removed dependencies

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # lkpg_cheapest_groceries
-Scrape common grocery stores in Linköping for cheapest prices
+Scrape common grocery stores in Linköping for cheapest prices.
+This script is in early development and currently only supports the following stores:
+
+* Willys
+* Hemköp
+
+Output of the program could look like:
+
+    user@pc ~/$ python3 scrape.py groceries.txt
+
+    Search results saved to:
+    receipt-2022-12-09 20:53:38.647578.txt
+
+    Receipt:
+    receipt-2022-12-09 20:53:38.647578.txt
+
+    ===========================================
+    Willys
+
+    Vetemjöl: 41.9kr 5kg
+    Köttbullar Fryst: 43.9kr 1kg
+
+    -------------------------------------------
+    Total Willys: 85.8kr
+    -------------------------------------------
+
+
+    ===========================================
+    Hemköp
+
+    Yoghurt Naturell: 27.5kr 1,5kg
+    Spaghetti Pasta: 33.5kr 1.8kg
+
+    -------------------------------------------
+    Total Hemköp: 61.0kr
+    -------------------------------------------
+
+
+    Total all: 146.8kr
+
+
+## Prerequisites
+
+Create a grocery list like `groceries.txt` that can look like:
+
+    Pasta
+    Yoghurt
+    Vetebullar
+    Broccoli
+
+Capital letters might be necessary, an easy fix is to go to one of the websites and copy-paste the correct name for the item into the text file.
+
+## Usage
+
+Simply have the script use your grocery list like `python3 scrape.py <LIST>` which in practice could look like:
+
+    python3 scrape.py groceries.txt
--- scrape.py
+++ scrape.py
@@ -1,8 +1,69 @@
-import argparse
 import re
-import time
+import requests
+import argparse
+import datetime
 
-from stores import *
+"""This scraper can only retrieve sites that does not rely on javascript to show the products.
+This will not work for the following stores:
+
+Ica, CityGross, Lidl, Coop
+
+But it will work for the following stores:
+
+Hemköp, Willys
+
+"""
+
+stores = {
+    # Tuesday December 06, 2022
+    "Hemköp": {
+        "url": "https://www.hemkop.se/sok?q=",
+        "headers": {"Accept-Language": "sv-se"},
+
+        # The search results
+        "products": r'data-testid="product-container".+?(?=data-testid="product-container")',
+
+        # Each product
+        "title": r'title="(.+?(?="))',
+        "brand": r'data-testid="display-manufacturer".+?(?=>)>(.+?(?=<\/span))',
+        "amount": r'data-testid="display-volume">(.+?(?=<\/span))',
+        "compare_price": r'compare-price">(.+?)(?=<)',
+        "promotion_compare_price": r'promotion-compare-price">(.+?)(?=<)',
+        "price": r'testid="price".*>(\d*,\d*)<span',
+    },
+
+    # Tuesday December 06, 2022
+    "Willys": {
+        "url": "https://www.willys.se/sok?q=",
+        "headers": {"Accept-Language": "sv-se"},
+
+        # The search results
+        "products": r'data-testid="product".+?(?=data-testid="product")',
+
+        # Each product
+        "title": r'Productstyles__StyledProductName.+?(?=>)>(.+?(?=<))',
+        "brand": r'Productstyles__StyledProductManufacturerBrand.+?(?=>)>(.+?(?=<))',
+        "amount": r'class="Productstyles__StyledProductDisplayVolume-.+?(?=>)>(\d+.+?(?=<))',
+        "compare_price": r'class="Productstyles__StyledProductComparePrice-.+?(?=>)>(.+?(?=<))',
+        "promotion_compare_price": r'class="Productstyles__StyledProductPromotionPriceInfo-.+?(?=>)>(.+?(?=<))',
+        "price": r'itemProp="price" content="(\d*,\d*)',
+    },
+
+    # "CityGross": {
+    #     "url": "https://www.citygross.se/sok?Q=",
+    #     "headers": {"Accept-Language": "sv",
+    #         "content": "initial-scale=1,width=device-width,shrink-to-fit=no,height=device-height,user-scalable=0,minimum-scale=1,maximum-scale=1,viewport-fit=cover"},
+
+    #     "products": r'class="l-column-30_xs-30_sm-20_md-15_lg-12_xlg-10-mobileGutter".+?(?=class="l-column-30_xs-30_sm-20_md-15_lg-12_xlg-10-mobileGutter")',
+    # }
+
+    # "Ica": {
+    #     "url": "https://www.ica.se/handla/sok/",
+    #     "headers": {"Accept-Language": "sv-se"},
+
+
+    # }
+}
 
 def get_args():
     """Retrieve arguments from terminal"""
@@ -20,12 +81,20 @@ def get_args():
 
     return args
 
+
+def regexget(regex, text):
+    """Return None if match is not found"""
+    return (re.findall(regex, text) or [None])[-1]
+
+
 def get_num(string):
+    """Retrieve number from a string"""
     if string is None:
         return None
 
     if isinstance(string, float):
         return string
+    
     result = re.findall(r'\d+,?\d+', string)
 
     if any(result):
@@ -34,10 +103,44 @@ def get_num(string):
     else:
         return None
 
+
 args = get_args()
 
 
-def process(results):
+def scrape(query):
+    """Scrape the websites and use Regex to find the information"""
+
+    result = {}
+
+    for store_name in stores:
+        store = stores[store_name]
+
+        response = requests.get(store["url"]+query, headers=store["headers"])
+
+        content = response.text
+
+        store_products = []
+
+        for item in re.findall(store["products"], content):
+            product = {}
+            for key in store:
+                if key in ("url", "headers", "products"):
+                    continue
+
+                product[key] = regexget(store[key], item)
+
+            store_products.append(product)
+
+        result[store_name] = store_products
+
+    return result
+
+
+def process(results_file):
+
+    with open(results_file, "r") as file:
+        results = eval("".join(file.readlines()))
+        file.close()
 
     tmp = {}
 
@@ -52,18 +155,14 @@ def process(results):
             for result in results[query][shop]:
 
                 if not args.cheapest_any_brand:
-
-                    try:
-                        if query not in result["title"]+result["brand"]:
+                    if query not in (result["title"] or " ")+(result["brand"] or " "):
                             continue
-                    except:
-                        continue
 
                 tmp_cheapest = result
 
                 tmp_cheapest["store"] = shop
 
-                values = ("original_price", "compare_price", "promo_compare_price")
+                values = ("price", "compare_price", "promotion_compare_price")
 
                 for i in values:
                     tmp_cheapest[i] = get_num(result[i])
@@ -75,7 +174,7 @@ def process(results):
             tmp[query] = cheapest[0]
 
             for item in cheapest[1:]:
-                if min(item["promo_compare_price"] or 1e3, item["compare_price"] or 1e3) <= min(tmp[query]["promo_compare_price"] or 1e3, tmp[query]["compare_price"] or 1e3):
+                if min(item["promotion_compare_price"] or 1e3, item["compare_price"] or 1e3) <= min(tmp[query]["promotion_compare_price"] or 1e3, tmp[query]["compare_price"] or 1e3):
                     tmp[query] = item
 
         else:
@@ -95,16 +194,16 @@ def process(results):
         a[item["store"]].append(item)
 
     sup_total = 0
-    receipt = f'{results["meta"]["date"]}'
-    # receipt = ""
-    # print(results)
+
+    receipt = f"\nReceipt:\n{results_file}\n"
+
     for store in a:
         total = 0
         receipt += "\n===========================================\n"
         receipt += store + "\n\n"
         for k in a[store]:
-            total += k["original_price"]
-            receipt += f'{k["title"]}: {k["original_price"]}kr\n'
+            total += k["price"]
+            receipt += f'{k["title"]}: {k["price"]}kr {k["amount"] or " "}\n'
 
         receipt += "\n-------------------------------------------\n"  \
             + f"Total {store}: {round(total, 2)}kr" \
@@ -117,51 +216,34 @@ def process(results):
     print(receipt)
 
 
-if args.from_history:
-    with open(args.from_history, "r") as file:
-        results = eval("".join(file.readlines()))
-        file.close()
-    process(results)
-
-else:
-
-    with open(args.groceries, "r") as file:
-        products = [i.replace("\n", "") for i in file.readlines()]
-        file.close()
-
+def main():
+    try:
+        with open(args.groceries, "r") as file:
+            products = [i.replace("\n", "") for i in file.readlines()]
+            file.close()
+    except FileNotFoundError:
+        print("You must specify a valid shopping list file!")
+        exit(1)
 
     results = {}
 
     for i, product in enumerate(products):
+        results[product] = scrape(product)
 
-        results[product] = {}
+        d = int((i+1)/len(products)*20)
+        print(f"Searching [{d*'='}{(20-d)*' '}] {int((i+1)/len(products)*100)}%", end="\r")
 
-        for shop in shops:
-            results[product][shop.__name__] = shop(product)
+    history_file = f'receipt-{datetime.datetime.now()}.txt'
 
-            if args.cheapest_any_brand:
-
-                ps = results[product][shop.__name__]
-
-                tmp_ps = []
-
-                for item in ps:
-                    tmp = item
-
-                    values = ("original_price", "compare_price", "promo_compare_price")
-
-                    for t in values:
-                        tmp[t] = get_num(item[t])
-
-                    tmp_ps.append(tmp)
-
-                results[product][shop.__name__] = tmp_ps
-
-        print(f"Searching {int((i+1)/len(products)*100)}%", end="\r")
-
-    results["meta"] = {"date": time.time()} 
-
-    process(results)
-
-    with open('history.txt','w') as data:
+    with open(history_file,'w') as data:
         data.write(str(results))
+        print(f"Search results saved to:                      \n{history_file}")
+        data.close()
+
+    return history_file
+
+
+if not args.from_history:
+    args.from_history = main()
+
+process(args.from_history)
