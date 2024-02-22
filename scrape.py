3fc5220239eb1dd28d8c406d0245db47316b23b0
Irreq <irreq@protonmail.com>
2024-02-22 09:43:37 +0100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -247,3 +247,190 @@ if not args.from_history:
     args.from_history = main()
 
 process(args.from_history)
+
+
+exit()
+
+import re
+import requests
+
+NAME: str = "Namn"
+PRICE: str = "Pris"
+
+stores = {
+    "Hemköp": {
+        "url": "https://www.hemkop.se/sok?q=",
+        "url_product": "https://www.hemkop.se/produkt/",
+        "headers": {"Accept-Language": "sv-se"},
+        # The search results
+        "products": r'data-testid="product-container".+?(?=data-testid="product-container")',
+        # Each product
+        "title": r'title="(.+?(?="))',
+        "brand": r'data-testid="display-manufacturer".+?(?=>)>(.+?(?=<\/span))',
+        "amount": r'data-testid="display-volume">(.+?(?=<\/span))',
+        "compare_price": r'compare-price">(.+?)(?=<)',
+        "promotion_compare_price": r'promotion-compare-price">(.+?)(?=<)',
+        "price": r'testid="price".*>(\d*,\d*)<span',
+        "json": r'"productSearchPageData":{"results":(.+?(?=,"sorts))',
+    },
+}
+
+
+def regexget(regex, text):
+    """Return None if match is not found"""
+    return (re.findall(regex, text) or [None])[-1]
+
+
+def get_num(string):
+    """Retrieve number from a string"""
+    if string is None:
+        return None
+
+    if isinstance(string, float):
+        return string
+
+    result = re.findall(r"\d+,?\d+", string)
+
+    if any(result):
+        return float(result[0].replace(",", "."))
+
+    else:
+        return None
+
+
+def download(query: str, store_name: str) -> str:
+    store = stores[store_name]
+
+    response = requests.get(store["url"] + query, headers=store["headers"])
+
+    content: str = response.text
+
+    return content
+
+
+def get_products(content: str, store_name: str) -> dict:
+    store_products = []
+
+    store = stores[store_name]
+
+    for item in re.findall(store["products"], content):
+        product = {}
+        for key in store:
+            if key in ("url", "headers", "products"):
+                continue
+
+            product[key] = regexget(store[key], item)
+
+        store_products.append(product)
+
+    product_json = regexget(store["json"], content)
+
+    orig_json = product_json
+
+    product_json = product_json.replace("null", "None")
+    product_json = product_json.replace("false", "False")
+    product_json = product_json.replace("true", "True")
+
+    return store_products, product_json, orig_json
+
+
+white_list = [
+    "101542823_ST",  # Trocadero gelehallon 220224
+]
+
+# %%
+query = "trocadero"
+store = "Hemköp"
+content = download(query, store, url="url")
+
+with open(f"{store}/{query}.html", "w") as f:
+    f.writelines(content)
+    f.close()
+
+with open(f"{store}/{query}.html", "r") as f:
+    content = "".join(f.readlines())
+    print(f.readlines())
+    f.close()
+
+
+# print(content)
+
+p, pjson, orig_json = get_products(content, store)
+with open(f"{store}/{query}.json", "w") as f:
+    f.writelines(orig_json)
+    f.close()
+
+pson = eval(pjson)
+print(pson)
+
+
+def parse_item(item):
+    price = float(item["priceValue"])
+    promoted = False
+    if item["potentialPromotions"]:
+        for prom in item["potentialPromotions"]:
+            if prom["price"]["value"] < price:
+                price = prom["price"]["value"]
+                promoted_qualifying_count = prom["qualifyingCount"]
+
+                promoted = True
+
+    compare_price = get_num(item["comparePrice"])
+    if promoted:
+        percentage = 100.0 * (1.0 - price / item["priceValue"])
+        promoted_price = price
+        saved = item["savingsAmount"]
+    else:
+        promoted_price = None
+        saved = 0
+        percentage = 0
+        promoted_qualifying_count = None
+
+    price = item["priceValue"]
+
+    manufacturer = item["manufacturer"]
+    name = item["name"]
+
+    condensed = {
+        "id": item["code"],
+        "name": name,
+        "manufacturer": manufacturer,
+        "price": price,
+        "unit": item["comparePriceUnit"],
+        "compare_price": compare_price,
+        "promoted": promoted,
+        "promoted_price": promoted_price,
+        "promoted_saved": saved,
+        "promoted_percentage": percentage,
+        "promoted_qualifying_count": promoted_qualifying_count,
+    }
+
+    return condensed
+
+
+def print_item(item):
+    if item["id"] not in white_list:
+        return
+    if item["promoted"]:
+        x = item["promoted_qualifying_count"]
+        if x > 1:
+            price_text = f'[{x}x{item["promoted_price"]}kr] '
+        else:
+            price_text = ""
+
+        price_text += f'{x*item["promoted_price"]}kr ({x*item["price"]}kr) -{x*item["promoted_saved"]}kr -{round(item["promoted_percentage"], 2)}%'
+
+    else:
+        price_text = f'{item["price"]}kr'
+
+    text = f"""
+{NAME}: {item["manufacturer"]} {item["name"]} ({item["compare_price"]}kr/{item["unit"]})
+{PRICE}: {price_text} 
+
+    """
+    print(text)
+
+
+for it in pson:
+    condensed = parse_item(it)
+    print_item(condensed)
